c2d43334b159de049bafb28d03e4f7646d78286b
kajela <titoyut56@gmail.com>
2023-06-11 05:11:20 -0400

    add the initial files

--- src/Parent.js
+++ src/Parent.js
@@ -6,10 +6,20 @@ function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
 
+  function handleChangeColor(){
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor);
+  }
+
+ function Child({onChangeColor}){
+  console.log(onChangeColor);
+  return <div onClick={onChangeColor} className="child" style={{ backgroundColor: "#FFF" }} />;
+ }
+
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child onChangeColor={handleChangeColor} />
+      <Child onChangeColor={handleChangeColor} />
     </div>
   );
 }
